4dd5372e2f7044a2da42acf6f48e8eeb23ab5f5c
ekolasky <ekolasky@gmail.com>
2024-09-02 00:09:43 +0000

    T

--- src/train.py
+++ src/train.py
@@ -96,7 +96,7 @@ def main():
 
     # Arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--epochs", type=int, default=100)
+    parser.add_argument("--epochs", type=int, default=10)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--batch_size", type=int, default=8)
     args = parser.parse_args()
